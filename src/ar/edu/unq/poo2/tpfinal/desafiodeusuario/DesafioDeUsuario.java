9cc110c1682793722b63ffff18f1f95cdb63209c
Martin <boidi.martinalejandro@gmail.com>
2023-12-05 19:58:52 -0300

    tp final poo2

---
+++ src/ar/edu/unq/poo2/tpfinal/desafiodeusuario/DesafioDeUsuario.java
@@ -0,0 +1,119 @@
+package ar.edu.unq.poo2.tpfinal.desafiodeusuario;
+
+
+
+import ar.edu.unq.poo2.tpfinal.desafio.Desafio;
+import ar.edu.unq.poo2.tpfinal.muestra.Muestra;
+import ar.edu.unq.poo2.tpfinal.usuario.Valoracion;
+import ar.edu.unq.poo2.tpfinal.usuario.Usuario;
+
+public class DesafioDeUsuario{
+
+	/* Esta clase modela el progreso de un desafio para un usuario concreto */
+	
+	private Desafio desafio;
+	private IEstadoDeDesafio estadoDeDesafio ;
+	private Valoracion gustoDeUsuario;
+	private int cantidadDeMuestrasPorRecolectar;
+	private float porcentajeDeCompletitud;
+	private Usuario usuario; 
+	
+	public DesafioDeUsuario(Desafio desafio , Usuario usuario) {
+		setEstadoDeDesafio(new EstadoAceptado());
+		this.desafio = desafio ;
+		this.cantidadDeMuestrasPorRecolectar = desafio.getCantidadDeMuestrasARecolectar();
+		this.porcentajeDeCompletitud = 0;
+		this.usuario = usuario;
+		this.gustoDeUsuario = Valoracion.NEUTRO; 
+	}
+	
+	
+	public void setGustoDeUsuario(Valoracion gustoDeUsuario) {
+		this.gustoDeUsuario = gustoDeUsuario;
+	}
+
+	public Desafio getDesafio1() {
+		return desafio;
+	}
+
+	public Valoracion getGustoDeUsuario() {
+		return gustoDeUsuario;
+	}
+	
+	public int getValorGustoDeUsuario() {
+		return gustoDeUsuario.getValorDeCalificacion();
+	}
+	
+	public int getCantidadDeMuestrasPorRecolectar() {
+		return cantidadDeMuestrasPorRecolectar;
+	}
+	
+	public boolean hayMuestrasPorRecolectar() {
+		return cantidadDeMuestrasPorRecolectar > 0;
+	}
+
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public IEstadoDeDesafio getEstadoDeDesafio() {
+		return estadoDeDesafio;
+	}
+
+	public void setEstadoDeDesafio(IEstadoDeDesafio estadoDeDesafio) {
+		this.estadoDeDesafio = estadoDeDesafio;
+		this.estadoDeDesafio.setDesafioDeUsuario(this);
+	}
+
+
+	public void recibirMuestra(Muestra muestra) {
+		this.estadoDeDesafio.recibirMuestra(muestra);
+	}
+
+
+	public float getPorcentajeDeCompletitud() {
+		return this.getEstadoDeDesafio().calcularPorcentajeDeCompletitud(getDesafio());
+				
+	} 	
+
+	protected boolean esValidaParaDesafio(Muestra muestra) {
+		/* Chekea que la muestra este dentro del area del desafio del DesafioDeUsuario */
+		return this.desafio.correspondeA(muestra);
+	}
+
+
+
+	public void setMuestrasPorRecolectar(int cantidadDeMuestras) {
+		this.cantidadDeMuestrasPorRecolectar = cantidadDeMuestras; 
+		
+	}
+	
+	public void agregarDesafioCompletoAListaDedesafiosCompletos(DesafioDeUsuario desafioDeUsuario) {
+		this.usuario.agregarDesafiosCompletos(desafioDeUsuario);
+	}
+
+
+	public boolean contieneAlDesafio(Desafio desafio) {
+		
+		return this.desafio == desafio;
+	}
+
+
+	public void validarMuestra(Muestra muestra) {
+		this.usuario.enviarMuestraAProyectos(muestra);
+		
+	}
+
+
+	public Desafio getDesafio() {
+		return desafio;
+	}
+
+
+	public void reducirCantidadDeMuestras() {
+		cantidadDeMuestrasPorRecolectar -= 1;
+	}
+
+	
+};
